53de7459d4650d707086a96be9b764a963eb4467
Kai Iemsawat <kaiiemsawat@gmail.com>
2023-09-04 23:32:24 -0400

    POST request

--- 02-springboot-restcrud-employee/src/main/java/com/springbootdemo/crudDemo/restCtrl/EmployeeCtrl.java
+++ 02-springboot-restcrud-employee/src/main/java/com/springbootdemo/crudDemo/restCtrl/EmployeeCtrl.java
@@ -3,9 +3,7 @@ package com.springbootdemo.crudDemo.restCtrl;
 import com.springbootdemo.crudDemo.entity.Employee;
 import com.springbootdemo.crudDemo.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -25,5 +23,29 @@ public class EmployeeCtrl {
     @GetMapping("/employees")
     public List<Employee> findAll() {
         return employeeService.findAll();
+    }
+
+//    add mapping for GET "/employees/{employeeId}
+    @GetMapping("/employees/{employeeId}") // {thisValue} == @PathVariable int thisValue
+    public Employee getEmployeeById(@PathVariable int employeeId) {
+        Employee theEmployee = employeeService.findById(employeeId);
+        if (theEmployee == null) {
+            throw  new RuntimeException("Employee id " + employeeId + " not found" );
+        }
+        return theEmployee;
+    }
+
+//    add mapping for POST "/employees" - add new employee
+    @PostMapping("/employees")
+    public Employee addEmployee(@RequestBody Employee theEmployee) {
+        theEmployee.setId(0);
+        /**
+        NOTE !  We set the id to 0 to force JPA/Hibernate to add/insert a new entry to the database.
+        Based on JPA logic, if id==0 then it will perform an add/insert in the database as opposed to an update.
+        We add this because for the case of "add", the user may mistakenly send over a non-zero value in
+        the JSON and this is not the desired data.
+         **/
+        Employee dbEmployee = employeeService.save(theEmployee); // save update
+        return  dbEmployee; // return updated employee
     }
 }
